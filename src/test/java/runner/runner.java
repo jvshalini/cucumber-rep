27c85a4bd6b9469710effdc586d23f0ea0e2597d
jvshalini <53205711+jvshalini@users.noreply.github.com>
2019-07-23 10:09:51 +0530

    Add files via upload

---
+++ src/test/java/runner/runner.java
@@ -0,0 +1,12 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="feature/feature1.feature", glue="stepdef",plugin="json:target\\Jsonreport.json")
+public class runner {
+
+}
